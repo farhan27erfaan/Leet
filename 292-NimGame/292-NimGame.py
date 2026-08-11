6a0f081f22eac64c1b77762df5c8f214907c1868
Samsudeen Farhan.F <139530154+farhan27erfaan@users.noreply.github.com>
2026-08-11 11:40:54 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.py
@@ -0,0 +1,11 @@
+# Last updated: 11/08/2026, 11:40:55
+class Solution(object):
+    def canWinNim(self, n):
+        if n%4==0:
+            n=False
+        else:
+            n=True
+        
+        return n
+    
+        
