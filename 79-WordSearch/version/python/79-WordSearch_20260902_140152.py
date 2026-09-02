36150f42283bd0eadc0cac5d91bf9d334a1798fb
Samsudeen Farhan.F <139530154+farhan27erfaan@users.noreply.github.com>
2026-09-02 14:01:53 +0530

    Create 79-WordSearch

---
+++ 79-WordSearch/version/python/79-WordSearch_20260902_140152.py
@@ -0,0 +1,25 @@
+# Last updated: 02/09/2026, 14:01:52
+1class Solution(object):
+2    def myAtoi(self, s):
+3    
+4        i = 0
+5        n = len(s)
+6        
+7        while i < n and s[i] == ' ':
+8            i += 1
+9        
+10        sign = 1
+11        if i < n and (s[i] == '-' or s[i] == '+'):
+12            sign = -1 if s[i] == '-' else 1
+13            i += 1
+14        
+15        result = 0
+16        while i < n and s[i].isdigit():
+17            result = result * 10 + int(s[i])
+18            if result * sign > 2**31 - 1:
+19                return 2**31 - 1
+20            if result * sign < -2**31:
+21                return -2**31
+22            i += 1
+23        
+24        return result * sign
