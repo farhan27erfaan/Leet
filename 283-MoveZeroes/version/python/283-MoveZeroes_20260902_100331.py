b41617b579f31359c7a3fb6a1f8d5a22979b1a2e
Samsudeen Farhan.F <139530154+farhan27erfaan@users.noreply.github.com>
2026-09-02 10:03:32 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/version/python/283-MoveZeroes_20260902_100331.py
@@ -0,0 +1,17 @@
+# Last updated: 02/09/2026, 10:03:31
+1class Solution(object):
+2    def findWords(self, words):
+3        m = {}
+4        for c in "qwertyuiop":
+5            m[c] = 1
+6        for c in "asdfghjkl":
+7            m[c] = 2
+8        for c in "zxcvbnm":
+9            m[c] = 3
+10        ans = []
+11        for w in words:
+12            lw = w.lower()
+13            r = m[lw[0]]
+14            if all(m[ch] == r for ch in lw):
+15                ans.append(w)
+16        return ans
