6f8e905a25a5ca5be49913b68ae89bce414db470
Samsudeen Farhan.F <139530154+farhan27erfaan@users.noreply.github.com>
2026-09-02 10:20:13 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/version/python/283-MoveZeroes_20260902_102011.py
@@ -0,0 +1,8 @@
+# Last updated: 02/09/2026, 10:20:11
+1class Solution(object):
+2    def nextGreatestLetter(self, letters, target):
+3        for letter in letters:
+4            if letter>target:
+5                return letter
+6        return letters[0]
+7        
